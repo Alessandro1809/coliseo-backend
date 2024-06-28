c52dbf92383430c721925bc03e93b0ca28296452
Alessandro1809 <dalessandro18dg@gmail.com>
2024-06-28 16:44:52 -0600

    Se quitaron las opciones useNewUrlPArser y useUnifiedTopologys por ser opciones de mongoose ya dprecadas

--- config/db.js
+++ config/db.js
@@ -1,11 +1,8 @@
 import mongoose from 'mongoose';
-//conexion a mongoDB
+//conexion a mongoDB se quito el objeto de configuracion debiodo a useNewUrlParser is a deprecated option as same useUnifiedTopology
 const connectDataBase= async ()=>{
     try {
-        const connectDB = await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        });
+        const connectDB = await mongoose.connect(process.env.MONGO_URI);
         const url= `${connectDB.connection.host}:${connectDB.connection.port}`;
         console.log(`Mongo db conectado en : ${url}`)
     } catch (error) {
